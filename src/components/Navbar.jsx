d847b442691e0874baed86255b64ce2c99fdd25b
konstapascal <pascalkonstantinos@gmail.com>
2022-02-23 22:31:23 +0100

    FEAT: Finished start page styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import UserInfo from './UserInfo';
 function Navbar() {
 	return (
 		<section className='bg-slate-900 border-b-4 border-slate-800'>
-			<nav className='container py-6 sm:py-8 max-w-5xl mx-auto flex  items-center text-center justify-center xl:justify-between'>
-				<div className='text-3xl md:text-4xl'>Lost in Translation</div>
+			<nav className='container py-6 sm:py-10 max-w-5xl mx-auto flex  items-center text-center justify-center xl:justify-between'>
+				<div className='text-4xl md:text-5xl font-sister'>Lost in Translation</div>
 				{/* <div className='flex text-lg text-blue-400 underline gap-4'>
 					<Link to='/'>Home</Link>
 					<Link to='/translation'>Translation</Link>
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,13 +1,13 @@
 function StartPage() {
 	return (
 		<section className='bg-slate-900 '>
-			<div className='container mx-auto max-w-5xl relative flex justify-center items-stretch text-center '>
-				<div className='flex items-center w-full justify-center relative flex-wrap xl:justify-between pt-6 md:pt-12 xl:pt-20 pb-24 md:pb-32 xl:pb-44'>
-					<img src='./Logo.png' className='w-80 sm:w-96' alt='' />
+			<div className='container  mx-auto max-w-5xl relative flex justify-center items-stretch text-center '>
+				<div className='flex items-center overflow-hidden w-full justify-center relative flex-wrap xl:justify-between pt-6 md:pt-12 xl:pt-20 pb-24 md:pb-32 xl:pb-44'>
+					<img src='./Logo.png' className='w-80' alt='' />
 					<div className='absolute xl:static w-full xl:w-auto z-10 py-10 px-4'>
-						<div className='absolute inset-0 bg-slate-800 opacity-90 xl:opacity-0 -z-10'></div>
-						<h1 className='text-5xl md:text-6xl font-semibold'>Lost in Translation</h1>
-						<p className='text-3xl md:text-4xl text-slate-300 text-right italic mt-2 '>
+						<div className='absolute inset-0 bg-slate-800 opacity-90  xl:opacity-0 -z-10'></div>
+						<h1 className='text-5xl md:text-6xl font-semibold  '>Lost in Translation</h1>
+						<p className='text-3xl md:text-4xl text-slate-300 text-center xl:text-right italic mt-2 '>
 							Get started
 						</p>
 					</div>
