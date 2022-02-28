6e721923b09dac6aea45795300a5e0355dfdd19e
konstapascal <pascalkonstantinos@gmail.com>
2022-02-28 23:17:59 +0100

    CHORE: Improved translation summaries

--- src/components/TranslationSummary.jsx
+++ src/components/TranslationSummary.jsx
@@ -7,16 +7,22 @@ function TranslationSummary({ translation }) {
 	return (
 		<div className='mt-8 first:mt-0 mx-4 xl:mx-0 bg-slate-800 border-b-4 md:border-b-8 border-slate-300 rounded-2xl shadow-xl py-8 px-4'>
 			<div>
-				{translationSigns.map((letter, idx) => (
-					<img
-						className='inline w-[50px]'
-						src={getSignSrc(letter)}
-						key={`${idx}-${letter}`}
-						alt=''
-					/>
-				))}
+				<h2 className='text-2xl font-semibold mb-2'>Translation Signs</h2>
+				<div>
+					{translationSigns.map((letter, idx) => (
+						<img
+							className='inline w-[50px]'
+							src={getSignSrc(letter)}
+							key={`${idx}-${letter}`}
+							alt=''
+						/>
+					))}
+				</div>
 			</div>
-			<p className='text-xl mt-4 text-slate-400'>{translation}</p>
+			<div>
+				<h2 className='text-2xl font-semibold mt-4 '>Translation</h2>
+				<p className='text-xl text-slate-400'>{translation}</p>
+			</div>
 		</div>
 	);
 }
