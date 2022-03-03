a826447ffaa01d2a0dd1d7ae7cca1b204cb357b5
konstapascal <pascalkonstantinos@gmail.com>
2022-03-03 11:09:35 +0100

    STORE: Changed from local to session storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,9 @@
 export function storageSave(key, value) {
-	localStorage.setItem(key, JSON.stringify(value));
+	sessionStorage.setItem(key, JSON.stringify(value));
 }
 
 export function storageRead(key) {
-	const data = localStorage.getItem(key);
+	const data = sessionStorage.getItem(key);
 	const parsedData = JSON.parse(data);
 
 	return parsedData;
